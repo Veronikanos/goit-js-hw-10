c85154c0f90eacc14f71d2e24b29de51d96656fe
Veronica <vveronikanika@gmail.com>
2022-11-19 19:18:01 +0200

    feat: add notiflix and info when more than 10 results

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-fetchCountries('Brunei');
+const inputForm = document.querySelector('input#search-box');
+
+inputForm.addEventListener('input', debounce(fetchCountry, DEBOUNCE_DELAY));
+
+function fetchCountry(event) {
+  const result = event.target.value.trim();
+  // console.log(result);
+
+  fetchCountries(result).then(data => {
+    // console.log(data.length);
+    if (data.length > 10) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+    } else if (data.length > 1 && data.length < 11) {
+      showShortResults(data);
+    } else {
+      showFullResults(data);
+    }
+  });
+}
+
+function showShortResults(countries) {
+  console.log(countries);
+}
+
+function showFullResults(countries) {
+  console.log(countries);
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,19 +1,24 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 export { fetchCountries };
 
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+const SEARCH_PARAM = 'name,capital,population,flags.svg,languages';
+
 function fetchCountries(name) {
-  console.log(`hello ${name}`);
-  fetch(`https://restcountries.com/v3.1/name/${name}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      console.log(response);
-      return response.json();
-    })
-    // .then(data => {
-    //   console.log(data);
-    // })
-    .catch(error => {
-      console.log('Something wrong 😣');
-    });
+  // console.log(`hello ${name}`);
+  return (
+    fetch(`${BASE_URL}/${name}?fields=${SEARCH_PARAM}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      // .then(data => {
+      //   console.log(data);
+      // })
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+      })
+  );
 }
