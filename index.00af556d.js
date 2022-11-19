45c05f395e88e38b091e411bfb2dbf74fcc67467
Veronikanos <49239848+Veronikanos@users.noreply.github.com>
2022-11-19 17:18:23 +0000

    Deploying to gh-pages from @ Veronikanos/goit-js-hw-10@c85154c0f90eacc14f71d2e24b29de51d96656fe 🚀

--- index.00af556d.js
+++
@@ -1,2 +0,0 @@
-var o;o="Brunei",console.log(`hello ${o}`),fetch(`https://restcountries.com/v3.1/name/${o}`).then((o=>{if(!o.ok)throw new Error(o.status);return console.log(o),o.json()})).catch((o=>{console.log("Something wrong 😣")}));
-//# sourceMappingURL=index.00af556d.js.map
--- index.90a04b4b.js
+++
@@ -1,2 +0,0 @@
-!function(){var o;o="Brunei",console.log("hello ".concat(o)),fetch("https://restcountries.com/v3.1/name/".concat(o)).then((function(o){if(!o.ok)throw new Error(o.status);return console.log(o),o.json()})).catch((function(o){console.log("Something wrong 😣")}))}();
-//# sourceMappingURL=index.90a04b4b.js.map
